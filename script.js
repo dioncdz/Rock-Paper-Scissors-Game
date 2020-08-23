79685c8cdf1fe52d5ba1bd70fe1441e486032a68
dioncdz <dioncadizjr@gmail.com>
2020-08-23 12:13:17 +0800

    Just edited some code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let choices = document.querySelectorAll('.buttons');
 let comp = document.querySelector('.computer-select');
 let result = document.querySelector('#result');
@@ -65,15 +64,10 @@ choices.forEach(choice => choice.addEventListener('click', (e)=>{
 
 
 /*****
-=======
-let userScore = 0, compScore = 0;
-
->>>>>>> 9a0d6c38e3ed4739fafdbfc8112ea7a01bcb654d
 function playRound(playerSelection, computerSelection) {
    let userScore = 0;
    let compScore = 0;
 
-<<<<<<< HEAD
    // IF USER IS ROCK
    if (playerSelection.toLowerCase() === 'rock' &&
       computerSelection === 'scissors') {
@@ -105,31 +99,10 @@ function playRound(playerSelection, computerSelection) {
    else if (playerSelection.toLowerCase() === computerSelection) {
       return 'Try Again!';
    }
-=======
-  // IF USER IS ROCK
-  if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-    userScore++;
-  } else if (playerSelection.toLowerCase() === 'rock' &&  computerSelection === 'paper') {
-    compScore++;
-  }
-    // IF USER IS SCISSORS
-  else if (playerSelection.toLowerCase() === 'scissors' &&  computerSelection === 'paper') {
-    userScore++;
-  } else if (playerSelection.toLowerCase() === 'scissors' &&  computerSelection === 'rock') {
-    compScore++;
-  }
-  // IF USER IS PAPER
-  else if (playerSelection.toLowerCase() === 'paper' &&  computerSelection === 'rock') {
-    userScore++;
-  } else if (playerSelection.toLowerCase() === 'paper' &&  computerSelection === 'scissors') {
-    compScore++;
-  } 
->>>>>>> 9a0d6c38e3ed4739fafdbfc8112ea7a01bcb654d
 
    console.log(userScore, compScore);
 }
 
-<<<<<<< HEAD
 function computerPlay() {
    let hand = Math.ceil(Math.random() * 3);
    if (hand === 1) {
@@ -157,43 +130,4 @@ function game() {
 }
 
 game();
-***/
-=======
-// GENERATE RANDOM COMPUTER CHOICE
-function computerPlay () {
-  let hand = Math.ceil(Math.random() * 3);
-  if (hand === 1) {
-    return 'rock';
-  } else if (hand === 2) {
-    return 'paper';
-  } else {
-    return 'scissors';
-  }
-}
 
-function game() {
-  let playerSelection;
-  let computerSelection;
-
-  for(let i = 0; i < 5; i++) {
-    playerSelection = prompt('Rock, paper or scissors?');
-    computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-  }
-  console.log(`Final score is user: ${userScore} | comp: ${compScore}`);
-}
-
-game()
-
-if(userScore > compScore) {
-  console.log('You won the game');
-} else if(userScore < compScore) {
-  console.log('Computer won the game');
-} else {
-  console.log(`It's a tie!`);
-}
-
-
-
-
->>>>>>> 9a0d6c38e3ed4739fafdbfc8112ea7a01bcb654d
